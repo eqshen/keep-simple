41590b16b058c30e8d7a0b3fff49c0795b6a7eb9
EQShen <87026928@qq.com>
2019-06-14 23:45:53 +0800

    remove

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRevelant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRevelant.java
@@ -11,28 +11,6 @@ import java.util.List;
  */
 public class ArrayRevelant {
     public static void main(String[] args) {
-        try{
-            System.out.println(111111);
-            throw new Exception("some error happened");
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("error");
-        }
     }
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2){
-        //首先交换两个数组，保证nums1.length <= nums2.length 恒成立
-        int m = nums1.length;
-        int n = nums2.length;
-        if(m > n){
-            int tmp[] = nums1;
-            nums1 = nums2;
-            nums2 = tmp;
-
-            m = nums1.length;
-            n = nums2.length;
-
-        }
-        return 0d;
-    }
 }
