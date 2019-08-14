57c5c33a5d0fe7360b2e47cd33cabbf65ea33a1b
eqshen <87026928@qq.com>
2019-08-14 17:21:27 +0800

    daily

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -306,4 +306,55 @@ public class ArrayRelevant extends BaseTest {
         }
         System.out.println();
     }
+
+    @Test
+    public void testSearch(){
+        int nums[]={4,5,6,7,8,1,2,3};
+        int nums2[]={3,1};
+        int target =8;
+        System.out.println(search(nums,target));
+    }
+
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return -1;
+        }
+
+        int left = 0;
+        int right = len - 1;
+        // 注意：这里是等于
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+
+            if (nums[mid] > nums[right]) {
+                // 前有序，包括中间数
+                // 6 7 8 9 1 2
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // 注意：写这个 if 语句的时候，要让分支和上面一样
+                // 后有序，包括中间数
+                // 6 7 1 2 3 4 5 6
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
 }
