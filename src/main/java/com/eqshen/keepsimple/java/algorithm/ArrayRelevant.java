9e00379d96864d3ddbb6412307c645036d63616e
eqshen <87026928@qq.com>
2019-08-15 13:43:59 +0800

    exercise 34

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -2,6 +2,7 @@ package com.eqshen.keepsimple.java.algorithm;
 
 import com.eqshen.keepsimple.java.BaseTest;
 import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
 
 import java.lang.reflect.Array;
 import java.util.*;
@@ -356,5 +357,67 @@ public class ArrayRelevant extends BaseTest {
             return left;
         }
         return -1;
+    }
+
+    /**
+     * 34
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     *
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     *
+     * 如果数组中不存在目标值，返回 [-1, -1]。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        if(nums.length == 0) return new int[]{-1,-1};
+        int first = -1,last = -1;
+        int left = 0;
+        int right = nums.length -1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            //不断放缩右指针
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        if(nums[left] == target){
+            first = left;
+        }
+
+        left = 0;
+        right=nums.length-1;
+        while(left < right){
+            int mid = left + (right - left+1)/2;
+            //不断放缩left指针
+            if(nums[mid]> target){
+                right = mid -1;
+            }else{
+                left = mid;
+            }
+        }
+        if(nums[right] == target){
+            last = right;
+        }
+
+        return new int[]{first,last};
+    }
+
+    @Test
+    public void testSearchRange(){
+        int nums[] = {5,7,7,8,8,10};
+        int target = 6;
+        int result[] = searchRange(nums,target);
+        for (int i : result) {
+            System.out.print(i+",");
+        }
+        System.out.println();
     }
 }
