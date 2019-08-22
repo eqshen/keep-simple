8824d49619387d3f556a97b1c5c0dcf98ccd972c
eqshen <87026928@qq.com>
2019-08-22 22:18:04 +0800

    exercise 42

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -713,4 +713,44 @@ public class ArrayRelevant extends BaseTest {
         System.out.println(firstMissingPositive(nums));
     }
 
+    /**
+     * 42
+     * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
+        int sum = 0;
+        //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
+        for (int i = 1; i < height.length - 1; i++) {
+            int max_left = 0;
+            //找出左边最高
+            for (int j = i - 1; j >= 0; j--) {
+                if (height[j] > max_left) {
+                    max_left = height[j];
+                }
+            }
+            int max_right = 0;
+            //找出右边最高
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] > max_right) {
+                    max_right = height[j];
+                }
+            }
+            //找出两端较小的
+            int min = Math.min(max_left, max_right);
+            //只有较小的一段大于当前列的高度才会有水，其他情况不会有水
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+    @Test
+    public void testTrap(){
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height));
+    }
+
 }
