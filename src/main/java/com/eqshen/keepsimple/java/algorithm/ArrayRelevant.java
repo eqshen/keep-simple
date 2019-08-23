e5d3882e1ec0b0515a7c2a88ff87536ab634dfab
eqshen <87026928@qq.com>
2019-08-23 13:37:21 +0800

    T42增加解法2

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -747,10 +747,36 @@ public class ArrayRelevant extends BaseTest {
         return sum;
     }
 
+    /**
+     * 42
+     * 动态规划解法
+     * @param height
+     * @return
+     */
+    public int trap2(int[] height){
+        int maxLeft[] = new int[height.length];
+        int maxRight[] = new int[height.length];
+        for (int i = 1; i < height.length ; i++) {
+            maxLeft[i] = Math.max(maxLeft[i - 1],height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0 ; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1],height[ i+1]);
+        }
+
+        int sum = 0;
+        for (int i = 1; i <height.length - 1 ; i++) {
+            int min = Math.min(maxLeft[i],maxRight[i]);
+            if(min > height[i]){
+                sum+= min - height[i];
+            }
+        }
+        return sum;
+    }
+
     @Test
     public void testTrap(){
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(trap(height));
+        System.out.println(trap2(height));
     }
 
 }
