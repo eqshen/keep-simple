b8c3fac7a480f059945fbf5e3d6afe52e56333e2
eqshen <87026928@qq.com>
2019-08-29 13:35:31 +0800

    44迭代解法

--- src/main/java/com/eqshen/keepsimple/java/algorithm/StringRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/StringRelevant.java
@@ -493,4 +493,59 @@ public class StringRelevant extends BaseTest {
         System.out.println(longestValidParentheses(s));
     }
 
+
+    /**
+     * 44
+     * 给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+     *
+     * '?' 可以匹配任何单个字符。
+     * '*' 可以匹配任意字符串（包括空字符串）。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/wildcard-matching
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param str
+     * @param pattern
+     * @return
+     */
+    boolean isMatch2(String str, String pattern) {
+        int s = 0, p = 0, match = 0, starIdx = -1;
+        //遍历整个字符串
+        while (s < str.length()){
+            // 一对一匹配，两指针同时后移。
+            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+                s++;
+                p++;
+            }
+            // 碰到 *，假设它匹配空串，并且用 startIdx 记录 * 的位置，记录当前字符串的位置，p 后移
+            else if (p < pattern.length() && pattern.charAt(p) == '*'){
+                starIdx = p;
+                match = s;
+                p++;
+            }
+            // 当前字符不匹配，并且也没有 *，回退
+            // p 回到 * 的下一个位置
+            // match 更新到下一个位置
+            // s 回到更新后的 match
+            // 这步代表用 * 匹配了一个字符
+            else if (starIdx != -1){
+                p = starIdx + 1;
+                match++;
+                s = match;
+            }
+            //字符不匹配，也没有 *，返回 false
+            else return false;
+        }
+
+        //将末尾多余的 * 直接匹配空串 例如 text = ab, pattern = a*******
+        while (p < pattern.length() && pattern.charAt(p) == '*')
+            p++;
+
+        return p == pattern.length();
+    }
+
+    @Test
+    public void testIsMatch2(){
+        System.out.println(isMatch2("aaeerereb","a*eb*****"));
+    }
 }
