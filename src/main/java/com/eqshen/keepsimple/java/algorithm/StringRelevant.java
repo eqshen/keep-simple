ddde9aadb3f15fc2c77f8e598a7af2c98efebe3b
eqshen <87026928@qq.com>
2019-09-18 01:15:41 +0800

    exercise 67

--- src/main/java/com/eqshen/keepsimple/java/algorithm/StringRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/StringRelevant.java
@@ -625,4 +625,27 @@ public class StringRelevant extends BaseTest {
     public void testGroupAnagrams(){
         System.out.println(groupAnagrams(new String[]{}));
     }
+
+    /**
+     * 67
+     * 给定两个二进制字符串，返回他们的和（用二进制表示）。
+     *
+     * 输入为非空字符串且只包含数字 1 和 0。
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int ca = 0;
+        for (int i = a.length() - 1, j = b.length() -1; i >= 0 || j >=0; i--,j--) {
+            int sum = ca;
+            sum += i>=0?a.charAt(i) - '0':0;
+            sum += j>=0?b.charAt(j) - '0':0;
+            sb.append(sum %2);
+            ca = sum/2;
+        }
+        sb.append(ca == 1?ca:"");
+        return sb.reverse().toString();
+    }
 }
