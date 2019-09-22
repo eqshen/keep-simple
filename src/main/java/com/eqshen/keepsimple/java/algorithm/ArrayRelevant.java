e8a359824546163658cbc9e5726d9a1def2d1118
eqshen <87026928@qq.com>
2019-09-22 19:45:09 +0800

    exercise 56

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -1125,4 +1125,69 @@ public class ArrayRelevant extends BaseTest {
         System.out.println(canJump(nums));
     }
 
+
+    /**
+     * 56
+     * 给出一个区间的集合，请合并所有重叠的区间。
+     *
+     * 示例 1:
+     *
+     * 输入: [[1,3],[2,6],[8,10],[15,18]]
+     * 输出: [[1,6],[8,10],[15,18]]
+     * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     * 示例 2:
+     *
+     * 输入: [[1,4],[4,5]]
+     * 输出: [[1,5]]
+     * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-intervals
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        if(n<=1){
+            return intervals;
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> res = new ArrayList<>();
+        int []preArray = intervals[0];
+        res.add(preArray);
+        for (int i = 1; i < n; i++) {
+            int []curArray = intervals[i];
+            if(curArray[0] <= preArray[1]){
+                int tmp[] = new int[]{preArray[0],Math.max(preArray[1],curArray[1])};
+                if(res.size() > 0){
+                    res.remove(res.size() -1);
+                }
+                res.add(tmp);
+                preArray = tmp;
+            }else{
+                res.add(curArray);
+                preArray = curArray;
+            }
+
+        }
+        return res.toArray(new int[0][]);
+    }
+
+    @Test
+    public void testMerge(){
+        int array[][]= new int[][]{{1,4},{5,6}};
+        for (int[] ints : merge(array)) {
+            for (int anInt : ints) {
+                System.out.print(anInt+",");
+            }
+            System.out.println();
+        }
+    }
 }
