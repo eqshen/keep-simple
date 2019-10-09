c6cb418232edaae7776ab67268e36ac760093e73
eqshen <87026928@qq.com>
2019-10-09 19:41:59 +0800

    tmp

--- src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
@@ -82,6 +82,47 @@ public class BasicRelevant extends BaseTest {
         }
     }
 
+    /**
+     * 63
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     *
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     *
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        obstacleGrid[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i]== 0&&obstacleGrid[0][i-1]==1)?0:1;
+        }
+        for (int i = 1; i < m; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0&&obstacleGrid[i-1][0]==1) ?0:1;
+        }
 
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if(obstacleGrid[i][j] == 0){
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                }else{
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+//        if(obstacleGrid[m-1][n-1] == 1){
+//            return 0;
+//        }
+        return obstacleGrid[m-1][n-1];
+    }
 
 }
