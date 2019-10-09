798ba6641e661d3102af72a5e5f87abd468fc5cf
eqshen <87026928@qq.com>
2019-10-10 00:20:57 +0800

    fix bug

--- src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
@@ -103,12 +103,15 @@ public class BasicRelevant extends BaseTest {
             return 0;
         }
         obstacleGrid[0][0] = 1;
-        for (int i = 1; i < n; i++) {
-            obstacleGrid[0][i] = (obstacleGrid[0][i]== 0&&obstacleGrid[0][i-1]==1)?0:1;
-        }
+
         for (int i = 1; i < m; i++) {
-            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0&&obstacleGrid[i-1][0]==1) ?0:1;
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0&&obstacleGrid[i-1][0]==1) ?1:0;
         }
+
+        for (int i = 1; i < n; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i]== 0&&obstacleGrid[0][i-1]==1)?1:0;
+        }
+
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
