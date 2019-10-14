8d5bc5aacc4007a9e8443198b72c6fba033c6617
eqshen <87026928@qq.com>
2019-10-14 13:56:32 +0800

    64

--- src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/BasicRelevant.java
@@ -128,4 +128,33 @@ public class BasicRelevant extends BaseTest {
         return obstacleGrid[m-1][n-1];
     }
 
+    /**
+     * 64
+     * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+     *
+     * 说明：每次只能向下或者向右移动一步。
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid){
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for (int i = 0; i < rows ; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(i == 0 && j == 0) continue;
+                else if(i == 0 ){
+                    //如果是第一行
+                    grid[i][j] = grid[i][j] + grid[i][j -1];
+                }else if(j == 0){
+                    //如果是第一列
+                    grid[i][j] = grid[i][j] + grid[i-1][j];
+                }else{
+                    //当前路径的最小值等于 当前位置值 + min(上方值，左方值)
+                    grid[i][j] = grid[i][j] + Math.min(grid[i][j-1],grid[i-1][j]);
+                }
+            }
+        }
+        return grid[rows-1][cols-1];
+    }
+
 }
