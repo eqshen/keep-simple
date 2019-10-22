69b13e6f672b7349474afb75f1363ff15260eb3b
eqshen <87026928@qq.com>
2019-10-22 18:08:03 +0800

    78解法1

--- src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
+++ src/main/java/com/eqshen/keepsimple/java/algorithm/ArrayRelevant.java
@@ -1374,4 +1374,39 @@ public class ArrayRelevant extends BaseTest {
         }
         return res;
     }
+
+    /**
+     * 78
+     * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     * [1,2,3]
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums.length == 0){
+            return result;
+        }
+        result.add(new ArrayList<>());
+        //逐个枚举，空集的幂集只有空集，每增加一个元素，让之前幂集中的每个集合，追加这个元素，就是新增的子集。
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> tmpSet = new ArrayList<>(result.get(i));//这里一定要new 拷贝一份
+                tmpSet.add(num);
+                result.add(tmpSet);
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testSubsets(){
+        int []nums = {1,2,3};
+        List<List<Integer>> result = subsets(nums);
+
+        for (List<Integer> integers : result) {
+            System.out.println(integers);
+        }
+    }
 }
